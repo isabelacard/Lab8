8e50a88fcba729dabe46462f06d1efe4b55616a0
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-05-09 17:05:47 -0500

    Se agrega los elementos por id de DOM

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// Seleccionar los elemento x id del DOM: ingresar-tarea, boton-agregar y lista-tareas
+
+document.getElementById('ingresar-tarea');
+const agregar = document.getElementById('boton-agregar');
+const lista = document.getElementById('lista-tareas');
+
+// Obtener tareas del localStorage
+function obtenerTareasLocalStorage() {
+}
+
+// Guardar tareas en localStorage
+function guardarTareasLocalStorage(tareas) {
+}
+
+// Renderizar la lista de tareas en el DOM
+function mostrarTareas() {
+}
+
+// Marcar la Tarea como completada
+function completarTarea(index) {
+}
+
+// Eliminar la Tarea correspondiente
+function eliminarTarea(index) {
+}
+
+// Crear una nueva Tarea
+function nuevaTarea() {
+}
+  
+// Escuchar el boton Agregar y en el evento click llamar a nuevaTarea
+
+// Escuchar el inputTarea y en el evento keypress con la tecla Enter 
+// llamar a nuevaTarea
+
+// Cargar tareas al iniciar con mostrarTareas
+
+
